6078034e6cd7abf9211b7d6bf45843954115bf5a
adrien mondiere <adrien.mondiere@epsi.fr>
2015-10-02 11:53:22 +0200

    register test

--- webservice/src/AppBundle/Controller/SecurityController.php
+++ webservice/src/AppBundle/Controller/SecurityController.php
@@ -1,46 +1,65 @@
 <?php
 
 namespace AppBundle\Controller;
+// namespace OC\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-/**
- * Security controller
- */
+use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class SecurityController extends Controller
 {
-    /**
-     * @Route("/login", name="login_route")
-     */
     public function loginAction(Request $request)
     {
-        $authenticationUtils = $this->get('security.authentication_utils');
+       // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+    if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+        return $this->redirectToRoute('audios_route');
+    }
+    
+    // Le service authentication_utils permet de récupérer le nom d'utilisateur
+    // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
+    // (mauvais mot de passe par exemple)
+    $authenticationUtils = $this->get('security.authentication_utils');
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render(
-            'AppBundle:Security:login.html.twig',
-            array(
-                // last username entered by the user
-                'last_username' => $lastUsername,
-                'error'         => $error,
-            )
-        );
+    return $this->render('AppBundle:Security:login.html.twig', array(
+      'last_username'   => $authenticationUtils->getLastUsername(),
+      'error'           => $authenticationUtils->getLastAuthenticationError(),
+    ));
     }
 
-    /**
-     * @Route("/login_check", name="login_check")
-     */
     public function loginCheckAction()
     {
         // this controller will not be executed,
         // as the route is handled by the Security system
+    }
+
+    /**
+     * Checks if login and password are correct
+     *
+     */
+    public function checkUserAction(Request $request)
+    {
+
+        $mail = $request->query->get('mail', '');
+        $password = $request->query->get('password', '');
+
+        if($mail == '' || $password == '')
+            return new Response(Response::HTTP_BAD_REQUEST); //400
+
+        // $hash = hash("sha1", $password);
+
+        $user = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findOneBy(array(
+            'email' => $mail,
+            'password' => $password
+        ));
+
+        if(!$user)
+            return new Response(Response::HTTP_BAD_REQUEST); //400
+
+        return new Response(Response::HTTP_OK); //200
     }
 }
